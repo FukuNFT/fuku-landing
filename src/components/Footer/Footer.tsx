ccf4f8748720b6de384809af8fdd67f8a092ac96
cds95 <c.sastropranoto@gmail.com>
2022-04-17 21:37:24 +0700

    align footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,24 +9,26 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <img src={FooterBackgroundLg} alt="fuku-footer-background" />
-      <div className="footer__content-container">
-        <div className="footer__content">
-          <h1 className="footer__header">Join our community</h1>
-          <div className="footer__content-right">
-            <SubscribeForm />
-            <div className="footer__social">
-              <div className="footer__social-list">
-                <SocialMedia
-                  className="footer__social-icon"
-                  link="https://twitter.com/fukunft"
-                  image={<Twitter />}
-                />
+      <div className="footer__content-max">
+        <div className="footer__content-container">
+          <div className="footer__content">
+            <h1 className="footer__header">Join our community</h1>
+            <div className="footer__content-right">
+              <SubscribeForm />
+              <div className="footer__social">
+                <div className="footer__social-list">
+                  <SocialMedia
+                    className="footer__social-icon"
+                    link="https://twitter.com/fukunft"
+                    image={<Twitter />}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="footer-bottom">
-          <FukuLogoWhite />
+          <div className="footer-bottom">
+            <FukuLogoWhite />
+          </div>
         </div>
       </div>
     </footer>
